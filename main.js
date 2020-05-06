d7a5af7497570f17562070d240452289951c0049
victo <victor.royer44@gmail.com>
2020-05-06 17:25:09 +0200

    final commit Partie 1

--- main.js
+++ main.js
@@ -8,8 +8,8 @@ function main(){
     let circle = new Circle(50);
     circle.setCenter(new Point(100,75));
     let rectangle = new Rectangle(150, 100);
-    rectangle.setCenter(new Point(150,30));
-    let shapes = [circle, rectangle];
+    // rectangle.setCenter(new Point(150,30));
+    let shapes = [circle];
     let canvas = document.getElementById("drawings");
     let context = canvas.getContext("2d");
     drawShapes(shapes, context);
